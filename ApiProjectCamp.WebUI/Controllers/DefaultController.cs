a89a0926cf6b4d6f945a076f269b40a528424ebe
Armağan AŞIK <140605164+ArmaganAsik@users.noreply.github.com>
2025-12-16 01:42:30 +0300

    Reservation process completed

--- ApiProjectCamp.WebUI/Controllers/DefaultController.cs
+++ ApiProjectCamp.WebUI/Controllers/DefaultController.cs
@@ -1,12 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiProjectCamp.WebUI.Dtos.ReservationDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Text;
 
 namespace ApiProjectCamp.WebUI.Controllers
 {
     public class DefaultController : Controller
     {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public DefaultController(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(CreateReservationDto createReservationDto)
+        {
+            createReservationDto.ReservationStatus = "Onay Bekliyor";
+
+            HttpClient client = _httpClientFactory.CreateClient();
+            string jsonData = JsonConvert.SerializeObject(createReservationDto);
+            StringContent stringContent = new StringContent(jsonData, Encoding.UTF8, "application/json");
+            HttpResponseMessage responseMessage = await client.PostAsync("https://localhost:7258/api/Reservations", stringContent);
+            if (responseMessage.IsSuccessStatusCode)
+            {
+                return Content("OK");
+                //return RedirectToAction("Index");
+            }
+            return BadRequest("Kayıt başarısız");
+            //return View();
         }
     }
 }
